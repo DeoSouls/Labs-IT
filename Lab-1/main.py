8fdc18e8400b4e84c5f51cfb324965483c810a32
VLD.oS <panteleev9696@mail.ru>
2023-02-13 00:36:28 +0300

    remove code, added values

--- Lab-1/main.py
+++ Lab-1/main.py
@@ -1,28 +1,14 @@
-sides = [3, 2, 4, 7, 5, 12, 11, 13, 15, 16, 14, 14]
+firstValue = input("Введите первый коэффициент: ");
+print("a:", firstValue);
 
-sides = sorted(sides, reverse=True)
+secondValue = input("Введите второй коэффициент: ");
+print("b:", secondValue);
 
-smax = 0
-for i in range(len(sides)):
-    for j in range(i + 1, len(sides)):
-        for k in range(j + 1, len(sides)):
-            a = sides[i]
-            b = sides[j]
-            c = sides[k]
-            if a + b > c and a + c > b and b + c > a:
-                p = (a + b + c) / 2
-                s = (p * (p - a) * (p - b) * (p - c)) ** (1 / 2)
-                if s > smax:
-                    smax = s
-
-print("Максимальная площадь треугольника", smax)
-
-
-привет
-
-
-t
+firthValue = input("Введите третий коэффициент: ");
+print("c:", firthValue);
 
 
 
-rette
+
+# сколько лет сколько зим как дела?
+# и хуй здесь с капустой мы ее спустим на люкс и безумство то густо то пусто
