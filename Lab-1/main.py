2bde96f0170c868e30174484eed9798d25f62231
VLD.oS <panteleev9696@mail.ru>
2023-02-13 10:25:13 +0300

    Acceptance testing

--- Lab-1/main.py
+++ Lab-1/main.py
@@ -7,6 +7,19 @@ print("b:", secondValue)
 thirdValue = input("Введите третий коэффициент: ")
 print("c:", thirdValue)
 
+print(f"\nКвадратное уравнение: ({firstValue})x^2 + ({secondValue})x + ({thirdValue}) = 0")
+
 disk = int(secondValue) ** 2 - 4 * int(firstValue) * int(thirdValue)
 
-print(disk)
+if -disk > 0:
+    print("Уравнение не имеет корней")
+else:
+    root1 = ((int(secondValue) * (-1)) + (round(disk)**0.5)) / (2 * int(firstValue))
+    root2 = ((int(secondValue) * (-1)) - (round(disk)**0.5)) / (2 * int(firstValue))
+
+    print("\nДействительные корни: ")
+    if root1 > 0:
+        print("\nX1: ", root1)
+
+    if root2 > 0:
+        print("\nX2: ", root2)
